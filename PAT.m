1d84d91ee3bbae60eac01f7e39418dc5f32162f6
Marios <mariolis1000@gmail.com>
2025-09-29 16:01:46 +0300

    completed PAT

--- PAT.m
+++ PAT.m
@@ -4,17 +4,29 @@ clearvars, close all
 t = linspace(0,5,100000);
 y0 =0;
 
+a = -1
+b = 2.5
+
+b = b*(-1);
+% IMPORTANT!
+% dy/dx =  ay - b
+%so in ex. dy/dx  = -2y + 5
+%b = -5 !!!!
+
+
+
 %dy = -y + 5;
 %dt = 1;
 
 figure;
 hold on
 
-for y0 = -10:5:10
-  y = (y0 - 5)*exp(-t) + 5;
+for y0 = -10:1:10
+  y = (y0 - (b/a))*exp(a*t) + (b/a);
   plot(t,y, 'linewidth', 2, "b.")
 endfor
 %
-title("y = (y0- 5)*ze^{-t} + 5")
+title("y = [y0 - (b/a)]*e^{at} + (b/a)")
 xlabel('t')
 ylabel('y')
+hold off
