3347b1583fd4b20eb0b1cdb5e5f0c425f34b6137
Marios <mariolis1000@gmail.com>
2025-10-13 16:34:40 +0300

    m

--- xwrizwmenwn.m
+++ xwrizwmenwn.m
@@ -1,29 +1,26 @@
-%This script relies on symbolic package
-%To run you need to have python installed
-%along with python's sympy package
-%to install on a system that already has python and octave
-%type on a terminal:
-%pip install sympy
-%then open octave, and in it's command widow type:
-%pkg install -forge symbolic
-%once these are done the current script should run
-
 clearvars, close all
-%pkg load symbolic
 
-%syms x y
-
-
-%x = linspace(-10,10,10000);
+%x = linspace(-2,2,10000);
 
 C = 1.0;
 
+
+%y =  1 -(x.^3 + (x.^2).*2  + x.*2 + 4).^0.5
+
 figure(1);
 hold on
-for C = -3:1:3
-  ezplot(@(x,y) -x.^3 + 3.*y - y.^3 +C)
-endfor
+%for C = -3:1:3
+  %ezplot(@(x,y) -x.^3 + 3.*y - y.^3 +C)
+  %plot(x, y)
+  %ezplot(@(x,y) y - 1 + sqrt(x.^3 + ((x.^2).*2)  + x.*2 + 4))
+  ezplot(@(x,y) y.^4 + y.*16 + x.^4 -(x.^2).*8 -17)
+%endfor
+%ezplot(@(x,y) y+1, [-2 2 -2 2])
 axis equal
+grid on
+set(gca, 'GridColor', [0,0,0])
+set(gca, 'GridAlpha', 0.3)
+set(gca, 'Layer', 'top')
 hold off
 
 
