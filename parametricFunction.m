d3676806382235332733ef60af49967279126812
Marios <mariolis1000@gmail.com>
2026-01-07 15:49:58 +0200

    m

--- parametricFunction.m
+++ parametricFunction.m
@@ -1,12 +1,18 @@
 clearvars, close all
 
-t = linspace(0,2*pi, 10000);
+%t = linspace(0,2*pi, 10000);
 
-f1 =  cos(t);
-f2 = sin(t);
+%f1 =  cos(t);
+%f2 = sin(t);
+
+%x = linspace(-5,5,1000);
+%y = linspace(-5,5,1000);
+[x y] = meshgrid(-1:0.02:1, -1:0.02:1 );
+z =  y.^2 -(x.^2 );
+
 
 figure(1);
-plot3(f1, f2, t, "b.")
+plot3(x, y, z, "b.")
 axis equal
 grid on
 xlabel('x')
@@ -17,13 +23,13 @@ set(gca, 'GridAlpha', 0.1)
 set(gca, 'Layer', 'top')
 
 
-t1 = linspace(-10,10,10000);
-x = t1;
-y = t1.^2;
-figure(2);
-hold on
-plot(x, y, "b.")
+%t1 = linspace(-10,10,10000);
+%x = t1;
+%y = t1.^2;
+%figure(2);
+%hold on
+%plot(x, y, "b.")
 %axis equal
-hold off
+%hold off
 
 
